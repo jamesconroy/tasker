82dc3a6eca9443e00d07d37f5835ebd19876b2bb
James Conroy <jamespc167@gmail.com>
2015-05-28 11:58:54 -0500

    restful API created

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,9 +1,50 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var Task = require('../models/Tasks');
+
+// GET /api/tasks
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  console.log(req.body);
+  Task.find(function(error, tasks) {
+    res.json(tasks);
+  })
+});
+
+// GET /api/tasks/:id
+router.get('/:id', function(req, res, next) {
+  Task.findById(req.params.id, function(err, task) {
+    res.json(task)
+  });
+});
+
+router.post('/', function(req, res, next) {
+  console.log(req.body);
+  Task.create(req.body, function(err, task) {
+    res.json(task);
+  });
+});
+
+router.put('/:id', function(req, res, next) {
+  console.log(req.body);
+  Task.findByIdAndUpdate(req.params.id, req.body, function(err, task) {
+    res.json({"message": "Your task has been updated"})
+  });
+});
+
+router.patch('/:id', function(req, res, next) {
+  console.log(req.body);
+  Task.findByIdAndUpdate(req.params.id, req.body, function(err, task) {
+    res.json({"message": "Your task has been updated"})
+  });
+});
+
+router.delete('/:id', function(req, res, next) {
+  Task.findByIdAndRemove(req.params.id, req.body, function(err, task) {
+    res.json({'message': "Your task has been deleted"});
+  });
 });
 
 module.exports = router;
